9d6a4dd6918907c8c4a482eea4ef6fde6e734c9a
Kinashrko <dhanukinash@gmail.com>
2025-05-26 01:00:19 +0530

    ✅ Update workflow to enforce mochawesome reporter in CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,25 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://admin.uat.loyaltybeta.cxforge.com', // Main app URL
-    experimentalSessionAndOrigin: true, // Required for cy.origin()
-    chromeWebSecurity: false, // Needed if there's cross-origin access or iframe usage
-    defaultCommandTimeout: 10000, // Wait time for DOM elements
-    pageLoadTimeout: 60000, // In case page takes time to load
-    video: false, // Turn this on if you want recordings
+    baseUrl: 'https://admin.uat.loyaltybeta.cxforge.com',
+    experimentalSessionAndOrigin: true,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    video: false,
+    screenshotOnRunFailure: true, // take screenshots on test failure
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results', 
+      overwrite: false,              
+      html: false,                   
+      json: true                   
+    },
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
+      return config;
     },
   },
 });
